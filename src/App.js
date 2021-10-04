11df70f87dc4d47ace18d5f4b34cc738586adea4
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-10-04 14:27:45 +0600

    fake data update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
